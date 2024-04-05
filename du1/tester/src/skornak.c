c1895d4e0473875a7a27aede4026d8c44fd137f2
Ondrej Škorňák <130708513+OndrejSkornak@users.noreply.github.com>
2024-04-05 22:20:15 +0200

    2

--- du1/tester/src/skornak.c
+++ du1/tester/src/skornak.c
@@ -3,18 +3,24 @@
 #include <stdio.h>
 #include <unistd.h>
 
+// Veľkosť buffra pre čítanie a zápis
 #define BUF_SIZE (1024*650)
 
+// Globálny buffer a mutex pre synchronizáciu
 char buffer[BUF_SIZE];
 pthread_mutex_t mutex = PTHREAD_MUTEX_INITIALIZER;
+// Podmienkové premenné pre signalizáciu medzi vláknami
 pthread_cond_t writer_cond = PTHREAD_COND_INITIALIZER;
 pthread_cond_t reader_cond = PTHREAD_COND_INITIALIZER;
+// Premenné pre sledovanie stavu čítania a ukončenia
 int readed = 0;
-int done = 0; // Flag to indicate completion
+int done = 0;
 
+// Funkcia pre vlákno zapisovača
 void *writer(void *data) {
     while (1) {
         pthread_mutex_lock(&mutex);
+        // Čakanie, kým nie je čo čítať
         while (readed == 0) {
             if (done) {
                 pthread_mutex_unlock(&mutex);
@@ -23,6 +29,7 @@ void *writer(void *data) {
             pthread_cond_wait(&writer_cond, &mutex);
         }
 
+        // Zápis dát z buffra na štandardný výstup
         int tmp = 0;
         while(tmp < readed) {
             int w = write(STDOUT_FILENO, buffer + tmp, readed - tmp);
@@ -33,30 +40,36 @@ void *writer(void *data) {
             tmp += w;
         }
 
+        // Resetovanie buffra a signalizácia čítaču
         readed = 0;
         pthread_cond_signal(&reader_cond);
         pthread_mutex_unlock(&mutex);
     }
 }
 
+// Funkcia pre vlákno čítača
 void *reader(void *data) {
     while (1) {
         pthread_mutex_lock(&mutex);
+        // Čakanie, kým nie je buffer plný
         while (readed > 0) {
             pthread_cond_wait(&reader_cond, &mutex);
         }
 
+        // Čítanie dát do buffra zo štandardného vstupu
         int r = read(STDIN_FILENO, buffer, BUF_SIZE);
         if (r < 0) {
             perror("Error reading");
             exit(EXIT_FAILURE);
         } else if (r == 0) {
-            done = 1; // Set done flag if end-of-file is reached
+            // Ukončenie, ak nie sú žiadne dáta na čítanie
+            done = 1;
             pthread_cond_signal(&writer_cond);
             pthread_mutex_unlock(&mutex);
             pthread_exit(NULL);
         }
         readed = r;
+        // Signalizácia zapisovaču
         pthread_cond_signal(&writer_cond);
         pthread_mutex_unlock(&mutex);
     }
@@ -65,6 +78,7 @@ void *reader(void *data) {
 int main() {
     pthread_t in, out;
 
+    // Vytvorenie vlákien
     if (pthread_create(&in, NULL, reader, NULL) != 0) {
         perror("Error creating reader thread");
         return EXIT_FAILURE;
@@ -75,6 +89,7 @@ int main() {
         return EXIT_FAILURE;
     }
 
+    // Čakanie na ukončenie vlákien
     if (pthread_join(in, NULL) != 0) {
         perror("Error joining reader thread");
         return EXIT_FAILURE;
